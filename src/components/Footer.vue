1f3194996e0138d51f32f377ab79852136cdb7ab
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-08 23:42:35 +0100

    Create Navbar & Footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,63 @@
+<template>
+  <footer class="section bg-light mt-4" id="footer">
+      <div class="container text-center">
+        <div class="row">
+            <div class="col-sm-4 footer-cta">
+                <h3>كويزيتو</h3>
+                <p class="mt-3 ml-1 text-muted">إقرأ .. إلعب واربح</p>
+                <p class="ml-1"><a class="btn btn-primary" href="https://quizzito.com/main/quizzito/join" target="_blank" onClick="gtag('event', 'signup_bottom', { 'event_category': 'conversion', 'event_label': 'singup'});">أطلب الآن →</a></p>        
+            </div>
+            <div class="col-sm-2">
+                <ul class="list-unstyled footer-links ml-1 px-0">
+                    <li><b> كن معنا </b></li>
+                    <li><a href="https://docs.google.com/forms/u/0/d/1Hm2XiFNGMIdXyBzNhGHH9eLbKaT7k854Qch6HG-Z8I4/edit" target="_blank"> شريكا </a></li>
+                    <li><a href="https://docs.google.com/forms/u/0/d/1Kypyzg7myBvPAp7dLthUgMqBLTxMT4Mz5jyL9ExBFVQ/edit" target="_blank"> راعيا</a></li>
+                    <li><a href="https://docs.google.com/forms/d/18n8dIGb61py5W6duAvAdaOGCn12E63biHlHzOdHOUlY/edit" target="_blank"> بائعا </a></li>
+                </ul>
+            </div>
+            <div class="col-sm-2">
+                <ul class="list-unstyled footer-links ml-1 px-0">
+                    <li><b> الإجتماعي </b></li>
+                    <li><a href="https://web.facebook.com/QuizzitoApp/" target="_blank">Facebook</a></li>
+                    <li><a href="https://twitter.com/quizzitoapp/" target="_blank">Twitter</a></li>
+                    <li><a href="https://www.youtube.com/watch?v=Lg-ZGpXAFnQ" target="_blank"  >Youtube</a></li>
+                    <li><a target = "_blank" href="https://www.instagram.com/quizzitoapp/">Instagram</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-2">
+                <ul class="list-unstyled footer-links ml-1 px-0">
+                    <li><b> روابط أخرى </b></li>
+                    <li><a href="https://quizzito.com/main/quizzito/other/terms_of_use/ar" target="_blank"> شروط الإستخدام </a></li>
+                    <li><a href="https://quizzito.com/main/quizzito/other/privacy_policy/ar" target="_blank">  الخصوصية</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-2">
+                <a href="#home" class="btn btn-sm btn-outline-primary ml-1">إلى الأعلى</a>
+            </div>
+            <div class="row mt-4 small justify-content-md-center" dir="ltr">
+                <div class="col-md-auto">
+                </div>
+                <div class="col-md-auto">  
+                    <i class="pe-7s-mail-open-file px-2"></i> info@quizzito.com
+                </div>
+            </div>
+            <div class="row mt-2">
+                <div class="col text-muted small text-center">
+                    نشكر كل عملائنا على ثقتهم بما نقدمه    
+                </div>
+            </div>
+        </div>
+      </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light cf-8377-area-26063">
+        <div class="container">
+            <a class="navbar-brand" href="https://quizzito.com/main/">
+                <h3 class="gradient-mask"><img width="50" src="https://quizzito.com/main/assets/images/quizzito_logo_nerd.png" alt="Quizzito Symbol">كويزيتو</h3>
+            </a>
+            <div class="d-inline-flex align-items-center justify-content-between">
+                <a href="https://api.whatsapp.com/send?phone=41765085874&amp;text=كوزيتو! هل يمكن أن تخبرني أكثر؟" class="mx-3" target="_blank" onclick="gtag('event', 'whatsapp', { 'event_category': 'conversion', 'event_label': 'singup'});"> <img class="whatslogo d-block d-lg-none d-xl-none" src="https://quizzito.com/main/assets/images/whats.png" alt="What's App Logo"> </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#site-nav" aria-controls="site-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="site-nav">
+                <ul class="navbar-nav text-sm-left mr-auto">
+                    <li class="nav-item">
+                    <a class="nav-link" href="https://quizzito.com/main/#features" >المميزات</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="https://quizzito.com/main/#pricing"  >العملاء</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="https://blog.quizzito.com/" target="_blank"  >المدونة</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="https://blog.quizzito.com/2019/10/20/classification-kids-book/" target="_blank"  >العناوين</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="https://family.quizzito.com/" target="_blank" >للعائلة <span class="badge badge-primary">جديد</span></a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="https://quizzito.com/main/quizzito/reward">الهدايا</a>
+                    </li>
+                    <li class="nav-item text-center">
+                    <a id="login_teacher" href="#" class="btn align-middle btn-outline-primary my-2 my-lg-0">الدخول</a>
+                    </li>
+                    <li class="nav-item text-center">
+                    <a href="https://quizzito.com/main/quizzito/join" class="btn align-middle btn-primary my-2 my-lg-0" target="_blank">التسجيل</a>
+                    </li>
+                    <li class="nav-item dropdown language-switch">
+                    <a class="nav-link" href="#" data-toggle="dropdown"><span class="pe-7s-global"></span>  عربي <span class="pe-2x pe-7s-angle-down"></span>  </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="https://quizzito.com/main/language/change/english">English</a>
+                        <a class="dropdown-item" href="https://quizzito.com/main/language/change/french">Français</a>
+                    </div>
+                    </li>
+                    <li class="nav-item d-none d-lg-block d-xl-block">
+                    <a href="https://api.whatsapp.com/send?phone=41765085874&amp;text=كوزيتو! هل يمكن أن تخبرني أكثر؟" target="_blank" class="ml-2" onclick="gtag('event', 'whatsapp', { 'event_category': 'conversion', 'event_label': 'singup'});"> <img width="30" class="whatslogo" src="https://quizzito.com/main/assets/images/whats.png" alt="What's App Logo"> </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
