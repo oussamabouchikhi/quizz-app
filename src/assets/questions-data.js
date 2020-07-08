2769877600f019d8c93bb9ad7defeff0f299645b
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-08 23:43:02 +0100

    Add development data

---
+++ src/assets/questions-data.js
@@ -0,0 +1,44 @@
+const questions = [
+    {
+      "question": "ماذا مَدَ الأسد كعلامة على صداقته مع الثعلب ؟",
+      "choice1": "ذيله",
+      "choice2": "لسانه",
+      "choice3": "يده",
+      "answer": "3",
+      "progressArrow": "486px"
+    },
+    {
+      "question": "لماذا كان الثعلب يتجول في الغابة ؟",
+      "choice2": "يبحث عن صديقه",
+      "choice3": "يبحث عن فريسة",
+      "choice1": "يبحث عن إبنه",
+      "answer": "3",
+      "progressArrow": "378px"
+    },
+    {
+      "question": "كيف كان الحيوان الغريب الذي رآه الثعلب ؟",
+      "choice2": "صغيرا و مخيفا",
+      "choice3": "كبيرا و مخيفا",
+      "choice1": "كبيرا و طيبا",
+      "answer": "3",
+      "progressArrow": "270px"
+    },
+    {
+      "question": "...كان لهذا الحيوان شعر طويل على",
+      "choice3": "رأسه و لحيته",
+      "choice2": "رجليه و رأسه",
+      "choice1": "جسمه كله",
+      "answer": "3",
+      "progressArrow": "162px"
+    },
+    {
+      "question": "بماذا شعر الثعلب منذ الوهلة الأولى التي رأى فيها الأسد ؟",
+      "choice1": "بالشفقة و الرأفة",
+      "choice2": "بالحزن الشديد",
+      "choice3": "بالخوف الشديد",
+      "answer": "3",
+      "progressArrow": "54px"
+    }
+];
+
+export default questions;
